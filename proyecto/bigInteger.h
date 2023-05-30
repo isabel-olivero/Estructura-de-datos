c55af0ffd6167ddc9b2fc02efc8bc39812f3b69b
isabel-olivero <124202028+isabel-olivero@users.noreply.github.com>
2023-05-30 07:09:59 -0500

    Add files via upload

--- proyecto/bigInteger.h
+++ proyecto/bigInteger.h
@@ -1,6 +1,6 @@
 /* Autor: Laura Isabel Olivero
 Fecha de creacion: 27 de abril 
-Ultima modificacion: 30 de abril
+Ultima modificacion: 30 de mayo
 
 Representacion BigIntegreer
 */
@@ -8,52 +8,49 @@ Representacion BigIntegreer
 #include <vector>
 #include <string>
 
-#ifndef __bigInteger_H
-#define __bigInteger_H
-
-#include "lista.h"
+#ifndef __BIGINTEGER_H
+#define __BIGINTEGER_H
 
 
 using namespace std;
 
-typedef int Elemento;
+
 
 class BigInteger{
 	
-	Lista l;
-	string s;
+	vector<int> big;
+	int size;
 	
 	public:
-		datos(Lista b){
-			this -> lista = b;
-		}
+		
 		//constructoras //
 		
-		BigInteger(string s); 
-		BigIntegerb(BigInteger big);
+		BigInteger(const string &); //~
+		BigInteger(BigInteger &big);//~
 		
 		//Modificadoras //
-		void add(BigInteger &a);
-		void product (BigInteger &a);
- 		void substract (BigInteger &a);
-		void quotient (BigInteger &a);
-		void remainder(BigInteger &a);
-		void pow (BigInteger &a);
+		void add(BigInteger &);//~
+		void product(BigInteger &);//~
+ 		void substract(BigInteger &);//~
+		void quotient(BigInteger &);//-
+		void remainder(BigInteger &);//-
+		void pow(BigInteger &);//-
 		
 		// Analizadoras //
-		string toString ();
-		BigInteger sumarListaValores(BigInteger &a, BigInteger &b); 
-		BigInteger MultiplicarListaValores(BigInteger &a,BigInteger &b);
+		string toString ();//~
+		int sumarListaValores(vector <BigInteger> &); //~
+		int MultiplicarListaValores(vector <BigInteger> &);//~
 		
 		//Sobrecargas//
-		BigInteger operator +(BigInteger);
-		BigInteger operator - (BigInteger);
-		BigInteger operator * (BigInteger);
-		BigInteger operator / (BigInteger);
-		BigInteger operator % (BigInteger);
-		bool operator == (BigInteger);
-		bool operator < (BigInteger);
-		bool operator <= (BigInteger);
+		int operator[](int);//~
+		BigInteger operator +(BigInteger &a);//~
+		BigInteger operator - (BigInteger &a);//~
+		BigInteger operator * (BigInteger &a);//-
+		BigInteger operator / (BigInteger &a);//-
+		BigInteger operator % (BigInteger &a);//-
+		bool operator == (BigInteger &a);//~
+		bool operator < (BigInteger &a);//~
+		bool operator <= (BigInteger &a);//~
 
 };
 
